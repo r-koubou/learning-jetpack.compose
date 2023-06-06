583a2265197d1545a71ad7471f73943ff9777299
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2023-06-06 12:18:37 +0900

    クエリのビルド関数追加

--- app/src/main/kotlin/com/example/learning/compose/demos/webapiclientdemo/advanced/webapiclient/WebApiRequest.kt
+++ app/src/main/kotlin/com/example/learning/compose/demos/webapiclientdemo/advanced/webapiclient/WebApiRequest.kt
@@ -2,6 +2,41 @@ package com.example.learning.compose.demos.webapiclientdemo.advanced.webapiclien
 
 class WebApiRequest(
     val url: String,
+    val query: Map<String, String> = emptyMap(),
     val method: String = "GET",
-)
-{}
+) {
+    fun getUrlWithQuery(): String {
+        if (query.isEmpty()) {
+            return url
+        }
+
+        val stringBuilder = StringBuilder(url)
+
+        stringBuilder.append("?")
+        return buildQuery(stringBuilder).toString()
+    }
+
+    fun getUrlQuery(): String {
+        if (query.isEmpty()) {
+            return ""
+        }
+        return buildQuery(StringBuilder()).toString()
+    }
+
+    private fun buildQuery(queryBuilder: StringBuilder): StringBuilder {
+        if (query.isEmpty()) {
+            return queryBuilder
+        }
+
+        queryBuilder.append("?")
+
+        for(entry in query.entries) {
+            queryBuilder.append("${entry.key}=${entry.value}")
+            if(entry != query.entries.last()) {
+                queryBuilder.append("&")
+            }
+        }
+
+        return queryBuilder
+    }
+}
